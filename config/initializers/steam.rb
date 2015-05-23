cfcc9b404aed183aa7b2a0384cf62b3b5a112550
Daniil Guzanov <melkor217@gmail.com>
2015-05-24 01:05:04 +0300

    Some doc

--- config/initializers/steam.rb
+++ config/initializers/steam.rb
@@ -1,2 +1,13 @@
-STEAM_CONFIG = YAML.load_file("#{Rails.root}/config/steamsecrets.yml")[Rails.env]
+steamcfg = YAML.load_file("#{Rails.root}/config/steamsecrets.yml")
+current_env = steamcfg[Rails.env]
+default_env = steamcfg['defaults']
+
+if current_env and current_env['key']
+  STEAM_CONFIG = current_env
+elsif default_env and default_env['key']
+  STEAM_CONFIG = default_env
+else
+  raise "Steam key is missing. Goto README.rdoc"
+end
+
 QWE_LOL = 1
