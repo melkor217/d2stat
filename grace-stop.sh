b8f51a9fe37c1ccfe96305bf9478df142b70f7fd
Daniil Guzanov <melkor217@gmail.com>
2015-06-26 23:02:12 +0300

    Fixed stop script

--- grace-stop.sh
+++ grace-stop.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 PROCLIST=`ps aux|grep side[k]iq\ 3`
-echo $PROCLIST
-PID=`echo $PROCLIST | awk '{ print $2 }'`
-kill -USR1 $PID
+echo "$PROCLIST"
+
+echo "$PROCLIST" | while read line
+do
+  PID=`echo $line | awk '{ print $2 }'`
+  kill -USR1 $PID
+  echo $PID
+done
